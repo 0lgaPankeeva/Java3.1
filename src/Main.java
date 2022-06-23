12e5b8a6d87fef32e479546a2878d629d112222c
Pankeeva Olga <0lga.pankeeva@mail.ru>
2022-06-23 23:22:22 +0400

    commit add

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 20; // колличество рублей для одной бонусной мили
+        int price = 40; // стоимость билета
+
+
+        System.out.println("колличество бонусных миль");
+        System.out.println(price/amount);
+
+
+    }
+}
